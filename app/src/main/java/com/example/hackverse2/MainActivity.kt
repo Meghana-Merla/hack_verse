6bef4de336947f7b5ea34b59b231d6d8c8f7677a
Meghana-Merla <24je0654@iitism.ac.in>
2024-12-18 17:55:08 +0530

    Main Activity

---
+++ app/src/main/java/com/example/hackverse2/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.hackverse2
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        title = "HackVerse" // Set title for the activity
+
+        // Open login activity
+        val buttonLogin: Button = findViewById(R.id.login_button)
+        buttonLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Open register activity
+        val buttonRegister: Button = findViewById(R.id.register_button)
+        buttonRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
